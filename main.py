9d12ecf604100a67d0d225f02f3efe16b6dcc21d
JulissaM07 <julissaking381@gmail.com>
2025-06-23 11:04:37 -0500

    Update Coffee Mechine

--- main.py
+++ main.py
@@ -1,3 +1,10 @@
 from menu import Menu
 from coffee_maker import CoffeeMaker
+
 from money_machine import MoneyMachine
+
+coffee_machine = Menu() 
+coffee_list = coffee_machine.menu
+print(coffee_list[0].name)
+
+
